5df1768c3f559ff053809043cc6c230de80616b0
hadiwyn <hadiwiyono1945@gmail.com>
2022-12-27 18:10:14 +0700

    firebase

--- lib/module/add_data/view/add_data_view.dart
+++ lib/module/add_data/view/add_data_view.dart
@@ -1,10 +1,23 @@
 import 'dart:ui';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import '../controller/add_data_controller.dart';
 
 class AddDataView extends StatefulWidget {
-  const AddDataView({Key? key}) : super(key: key);
+  var db = FirebaseFirestore.instance;
+
+  Future<FirebaseApp> _initializeFirebase() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
+  }
+
+  TextEditingController nama_barangC = TextEditingController();
+  TextEditingController deskripsiC = TextEditingController();
+  TextEditingController nama_penemuC = TextEditingController();
+  TextEditingController noTlpC = TextEditingController();
+  TextEditingController keyC = TextEditingController();
 
   Widget build(context, AddDataController controller) {
     controller.view = this;
@@ -14,16 +27,26 @@ class AddDataView extends StatefulWidget {
         title: const Text("AddData"),
         actions: const [],
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [judul(), content()],
-          ),
-        ),
+      body: FutureBuilder(
+        future: _initializeFirebase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [judul(), content()],
+                ),
+              ),
+            );
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
@@ -54,53 +77,73 @@ class AddDataView extends StatefulWidget {
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               TextFormField(
+                controller: nama_barangC,
                 decoration: InputDecoration(
                   icon: Icon(Icons.person),
                   hintText: 'What do people call you?',
-                  labelText: 'Name *',
+                  labelText: 'Nama Barang',
+                ),
+              ),
+              TextFormField(
+                controller: nama_penemuC,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'What do people call you?',
+                  labelText: 'Nama Penemu',
+                ),
+              ),
+              TextFormField(
+                controller: noTlpC,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'What do people call you?',
+                  labelText: 'Nomor Telepon',
                 ),
               ),
               TextFormField(
                 decoration: InputDecoration(
                   icon: Icon(Icons.person),
                   hintText: 'What do people call you?',
-                  labelText: 'Name *',
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: 'What do people call you?',
-                  labelText: 'Name *',
-                ),
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: 'What do people call you?',
-                  labelText: 'Name *',
+                  labelText: 'Foto',
                 ),
               ),
               ElevatedButton(onPressed: () {}, child: Text('Upload Foto')),
               TextFormField(
+                controller: deskripsiC,
                 decoration: InputDecoration(
                   icon: Icon(Icons.person),
                   hintText: 'What do people call you?',
-                  labelText: 'Name *',
+                  labelText: 'Deskripsi',
                 ),
               ),
               TextFormField(
+                controller: keyC,
                 decoration: InputDecoration(
                   icon: Icon(Icons.person),
                   hintText: 'What do people call you?',
-                  labelText: 'Name *',
+                  labelText: 'Kunci Penghapusan',
                 ),
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  ElevatedButton(onPressed: () {}, child: Text('Tambah')),
-                  ElevatedButton(onPressed: () {}, child: Text('Batal')),
+                  ElevatedButton(onPressed: () {}, child: Text('Cancel')),
+                  ElevatedButton(
+                      onPressed: () {
+                        final user = <String, dynamic>{
+                          "nama_barang": "Jam Tangan",
+                          "nama_penemu": "Yusup",
+                          "nomor_telepon": "082237422743",
+                          "deskripsi": "barang Hilang",
+                          "key": "aku sayang ibu"
+                        };
+
+// Add a new document with a generated ID
+                        db.collection("Barang_Hilang").add(user).then(
+                            (DocumentReference doc) => print(
+                                'DocumentSnapshot added with ID: ${doc.id}'));
+                      },
+                      child: Text('Tambah')),
                 ],
               )
             ],
